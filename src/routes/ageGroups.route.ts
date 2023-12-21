d093c1694bbe8b0efde2b43c3165d91028729de3
djelavic_ag <djelavic_ag@medable.com>
2023-12-21 16:28:33 +0100

    Append middlewares to routes

--- src/routes/ageGroups.route.ts
+++ src/routes/ageGroups.route.ts
@@ -1,9 +1,15 @@
 import express from 'express'
 import AgeGroupController from '../controllers/ageGroup.controller'
+import { checkToken, isAdmin } from '../middlewares'
 
 const router = express.Router()
 
-router.post('/create', AgeGroupController.createAgeGroup)
-router.patch('/:id/update', AgeGroupController.updateAgeGroup)
+router.post('/create', checkToken, isAdmin, AgeGroupController.createAgeGroup)
+router.patch(
+  '/:id/update',
+  checkToken,
+  isAdmin,
+  AgeGroupController.updateAgeGroup,
+)
 
 export default router
--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -1,11 +1,32 @@
 import express from 'express'
 import CommentsController from '../controllers/comments.controller'
+import { checkToken, isAdmin, isVerified } from '../middlewares'
 
 const router = express.Router()
 
-router.get('/:classId', CommentsController.getClassComments)
-router.post('/create/:classId', CommentsController.createComment)
-router.patch('/update/:commentId', CommentsController.updateComment)
-router.delete('/delete/:commentId', CommentsController.deleteComment)
+router.get(
+  '/:classId',
+  checkToken,
+  isAdmin,
+  CommentsController.getClassComments,
+)
+router.post(
+  '/create/:classId',
+  checkToken,
+  isVerified,
+  CommentsController.createComment,
+)
+router.patch(
+  '/update/:commentId',
+  checkToken,
+  isVerified,
+  CommentsController.updateComment,
+)
+router.delete(
+  '/delete/:commentId',
+  checkToken,
+  isVerified,
+  CommentsController.deleteComment,
+)
 
 export default router
