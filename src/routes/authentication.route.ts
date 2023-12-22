b0830ac37a4eeb07c6781622a63e03af45a59f59
djelavic_ag <djelavic_ag@medable.com>
2023-12-22 16:49:08 +0100

    add swagger documentation and route descriptions

--- src/routes/authentication.route.ts
+++ src/routes/authentication.route.ts
@@ -2,9 +2,90 @@ import express from 'express'
 import AuthController from '../controllers/auth.controller'
 
 const router = express.Router()
+/**
+ * @swagger
+ * tags:
+ *   name: Authentication/authorization endpoints
+ *   description: API endpoints for authentication/authorization
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a user
+ *     tags: [Authentication/authorization endpoints]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: User registered
+ *       '400':
+ *         description: Schema validation error
+ *       '500':
+ *         description: Internal server error
+ */
 
 router.post('/register', AuthController.register)
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Log in
+ *     tags: [Authentication/authorization endpoints]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: User logged in
+ *       '400':
+ *         description: Schema validation error
+ *       '500':
+ *         description: Internal server error
+ */
+
 router.post('/login', AuthController.login)
+
+/**
+ * @swagger
+ * /auth/verify:
+ *   get:
+ *     summary: Verify a user
+ *     tags: [Authentication/authorization endpoints]
+ *     parameters:
+ *       - in: query
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '201':
+ *         description: User verified
+ *       '500':
+ *         description: Internal server error
+ */
+
 router.get('/verify', AuthController.verify)
 
 export default router
--- src/routes/sportClasses.route.ts
+++ src/routes/sportClasses.route.ts
@@ -3,14 +3,88 @@ import SportClassesController from '../controllers/sportClasses.controller'
 import { checkToken, isAdmin, isVerified } from '../middlewares'
 
 const router = express.Router()
+/**
+ * @swagger
+ * tags:
+ *   name: Sport Classes
+ *   description: API endpoints for managing sport classes
+ */
+
+/**
+ * @swagger
+ * /sport-classes/create:
+ *   post:
+ *     summary: Create a new sport class
+ *     tags: [Sport Classes]
+ *     security:
+ *       - bearerAuth: []
+ *       - Admin only
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Sport class created successfully
+ */
 
 router.post('/create', checkToken, isAdmin, SportClassesController.createClass)
+
+/**
+ * @swagger
+ * /sport-classes/{id}/update:
+ *   patch:
+ *     summary: Update a sport class by ID
+ *     tags: [Sport Classes]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     security:
+ *       - bearerAuth: []
+ *       - Admin only
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       '201':
+ *         description: Sport class updated successfully
+ */
+
 router.patch(
   '/:id/update',
   checkToken,
   isAdmin,
   SportClassesController.updateClass,
 )
+
+/**
+ * @swagger
+ * /sport-classes:
+ *   get:
+ *     summary: Get all sport classes
+ *     tags: [Sport Classes]
+ *     security:
+ *       - bearerAuth: []
+ *       - Verified only
+ *     responses:
+ *       '200':
+ *         description: Successful operation
+ */
+
 router.get('/', checkToken, isVerified, SportClassesController.getClasses)
 
 export default router
