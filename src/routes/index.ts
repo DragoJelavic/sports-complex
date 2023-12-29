9c8286fb9c6aa790647d2a86fe0e975ba425f3fb
djelavic_ag <djelavic_ag@medable.com>
2023-12-29 19:49:47 +0100

    add route manager

--- src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ageGroups.route'
-export * from './authentication.route'
-export * from './sportClasses.route'
-export * from './sports.route'
-export * from './users.route'
-export * from './comments.route'
---
+++ src/routes/routeManager.ts
@@ -0,0 +1,15 @@
+import authRoutes from './authentication.route'
+import sportsRoutes from './sports.route'
+import ageGroupRoutes from './ageGroups.route'
+import sportClassesRoute from './sportClasses.route'
+import userRoute from './users.route'
+import commentsRoute from './comments.route'
+
+export default {
+  authRoutes,
+  sportsRoutes,
+  ageGroupRoutes,
+  sportClassesRoute,
+  userRoute,
+  commentsRoute,
+}
